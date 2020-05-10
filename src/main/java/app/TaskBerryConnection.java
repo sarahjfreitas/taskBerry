56f9c29e7bdfe7005b52cd16f7b18c3f4560026e
Sarah Freitas <sarahjfreitas@gmail.com>
2020-05-09 21:19:09 -0300

    docs update

--- src/main/java/app/TaskBerryConnection.java
+++ src/main/java/app/TaskBerryConnection.java
@@ -3,12 +3,12 @@ package app;
 import org.sql2o.Sql2o;
 
 public class TaskBerryConnection {
-    private static Sql2o sql2o;
+    private static Sql2o connection;
 
     public static Sql2o get(){
-        if(sql2o == null){
-            sql2o = new Sql2o("jdbc:sqlite:./taskberry.db",null,null);
+        if(connection == null){
+            connection = new Sql2o("jdbc:sqlite:./taskberry.db",null,null);
         }
-        return sql2o;
+        return connection;
     }
 }
